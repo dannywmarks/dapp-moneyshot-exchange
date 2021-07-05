37a9dbb5e886fae08f95ced2bd861b5179ecdd96
Danny Marks <dannywmarks@gmail.com>
2021-07-05 09:22:00 -0700

    deployment commit

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -1,14 +1,11 @@
 const Token = artifacts.require("Token");
 const Bar = artifacts.require("Bar");
 
-const ETHER_ADDRESS = "0x0000000000000000000000000000000000000000";
-
-// --- Helper function to convert big numbers ---
-
+// Utils
+const ETHER_ADDRESS = "0x0000000000000000000000000000000000000000"; // Ether token deposit address
 const ether = (n) => {
   return new web3.utils.BN(web3.utils.toWei(n.toString(), "ether"));
 };
-
 const tokens = (n) => ether(n);
 
 const wait = (seconds) => {
@@ -18,30 +15,30 @@ const wait = (seconds) => {
 
 module.exports = async function (callback) {
   try {
-    console.log("script running...");
+    // Fetch accounts from wallet - these are unlocked
     const accounts = await web3.eth.getAccounts();
 
-    // Fetch the delayed token
+    // Fetch the deployed token
     const token = await Token.deployed();
-    console.log("Token Fetched", token.address);
+    console.log("Token fetched", token.address);
 
-    // Fetch the deployed bar
+    // Fetch the deployed exchange
     const bar = await Bar.deployed();
-    console.log("Bar Fetched", bar.address);
+    console.log("Bar fetched", bar.address);
 
     // Give tokens to account[1]
     const sender = accounts[0];
     const receiver = accounts[1];
-    let amount = web3.utils.toWei("10000", "ether"); // 10,000
+    let amount = web3.utils.toWei("10000", "ether"); // 10,000 tokens
 
     await token.transfer(receiver, amount, { from: sender });
     console.log(`Transferred ${amount} tokens from ${sender} to ${receiver}`);
 
-    //Set up bar users
+    // Set up bar users
     const user1 = accounts[0];
     const user2 = accounts[1];
 
-    // User1 Deposits Ether
+    // User 1 Deposits Ether
     amount = 1;
     await bar.depositEther({ from: user1, value: ether(amount) });
     console.log(`Deposited ${amount} Ether from ${user1}`);
@@ -55,8 +52,9 @@ module.exports = async function (callback) {
     await bar.depositToken(token.address, tokens(amount), { from: user2 });
     console.log(`Deposited ${amount} tokens from ${user2}`);
 
-    /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
     // Seed a Cancelled Order
+    //
 
     // User 1 makes order to get tokens
     let result;
@@ -70,28 +68,34 @@ module.exports = async function (callback) {
     );
     console.log(`Made order from ${user1}`);
 
-    // User 1 cancels order
+    // User 1 cancells order
     orderId = result.logs[0].args.id;
     await bar.cancelOrder(orderId, { from: user1 });
     console.log(`Cancelled order from ${user1}`);
 
-    ///////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
     // Seed Filled Orders
     //
 
-    // // User 1 makes order
-    // result = await bar.makeOrder(token.address, tokens(100), ETHER_ADDRESS, ether(0.1), { from: user1 })
-    // console.log(`Made order from ${user1}`)
+    // User 1 makes order
+    result = await bar.makeOrder(
+      token.address,
+      tokens(100),
+      ETHER_ADDRESS,
+      ether(0.1),
+      { from: user1 }
+    );
+    console.log(`Made order from ${user1}`);
 
-    // // user 2 fill anothe rorder
-    // orderId = results[0].args.id
-    // await bar.fillOrder(orderId, { from: user2 })
-    // console.log(`Filled order from ${user1}`)
+    // User 2 fills order
+    orderId = result.logs[0].args.id;
+    await bar.fillOrder(orderId, { from: user2 });
+    console.log(`Filled order from ${user1}`);
 
     // Wait 1 second
     await wait(1);
 
-    // user1 make another order
+    // User 1 makes another order
     result = await bar.makeOrder(
       token.address,
       tokens(50),
@@ -101,7 +105,7 @@ module.exports = async function (callback) {
     );
     console.log(`Made order from ${user1}`);
 
-    // user 2 fill anothe rorder
+    // User 2 fills another order
     orderId = result.logs[0].args.id;
     await bar.fillOrder(orderId, { from: user2 });
     console.log(`Filled order from ${user1}`);
@@ -109,7 +113,7 @@ module.exports = async function (callback) {
     // Wait 1 second
     await wait(1);
 
-    // user1 make another order
+    // User 1 makes final order
     result = await bar.makeOrder(
       token.address,
       tokens(200),
@@ -119,7 +123,7 @@ module.exports = async function (callback) {
     );
     console.log(`Made order from ${user1}`);
 
-    // user 2 fill anothe rorder
+    // User 2 fills final order
     orderId = result.logs[0].args.id;
     await bar.fillOrder(orderId, { from: user2 });
     console.log(`Filled order from ${user1}`);
@@ -127,28 +131,33 @@ module.exports = async function (callback) {
     // Wait 1 second
     await wait(1);
 
-    /////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
     // Seed Open Orders
+    //
+
+    // User 1 makes 10 orders
     for (let i = 1; i <= 10; i++) {
-      result =
-        (await bar.makeOrder(
-        token.address, tokens(10*i), ETHER_ADDRESS, ether(0.01), {from: user1}
-        ));
+      result = await bar.makeOrder(
+        token.address,
+        tokens(10 * i),
+        ETHER_ADDRESS,
+        ether(0.01),
+        { from: user1 }
+      );
       console.log(`Made order from ${user1}`);
       // Wait 1 second
       await wait(1);
     }
 
-    //User 2 makes 10 order
+    // User 2 makes 10 orders
     for (let i = 1; i <= 10; i++) {
-      result =  (await bar.makeOrder(
+      result = await bar.makeOrder(
         ETHER_ADDRESS,
         ether(0.01),
         token.address,
         tokens(10 * i),
         { from: user2 }
-      ));
-      
+      );
       console.log(`Made order from ${user2}`);
       // Wait 1 second
       await wait(1);
@@ -156,5 +165,6 @@ module.exports = async function (callback) {
   } catch (error) {
     console.log(error);
   }
+
   callback();
 };
--- truffle-config.js
+++ truffle-config.js
@@ -2,16 +2,34 @@ require("babel-register");
 require("babel-polyfill");
 require("dotenv").config();
 
+const privateKeys = process.env.PRIVATE_KEYS || ''
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+
+
 module.exports = {
+  
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // match any network id
     },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(
+          // Private Key
+          privateKeys.split(','), // array of private keys
+          // Url to an Etherum
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+        );
+      },
+      network_id: 42,
+      gas: 5000000,
+      gasPrice: 25000000000,
+    },
   },
   contracts_directory: "./src/contracts/",
-  contracts_build_directory: "./src/abis",
+  contracts_build_directory: "./src/abis/",
 
   // Configure your compilers
   compilers: {
